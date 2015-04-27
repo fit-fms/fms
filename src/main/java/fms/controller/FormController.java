4b35c6716cddbffb4a0a4a5f80308b75344f9147
Michal Kocanek <kocanmi4@fit.cvut.cz>
2015-04-27 20:21:55 +0200

    form controller

--- src/main/java/fms/controller/FormController.java
+++ src/main/java/fms/controller/FormController.java
@@ -6,8 +6,10 @@
 package fms.controller;
 
 import fms.business.archetype.Archetype;
+import fms.business.form.DigitalForm;
 import fms.business.form.Form;
 import fms.business.service.ArchetypeService;
+import fms.business.service.FormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 /**
  *
@@ -34,24 +37,37 @@ public class FormController {
     
     
     @Autowired
-    private ArchetypeService service;
-    //private GWTForm gwt;
-    private Archetype arch;
-    private Form form;
+    private FormService formService;
+    @Autowired
+    private ArchetypeService archService;
+    
     
     @RequestMapping(value = "/form/{formUrl}", method = RequestMethod.GET)
     public String displayForm(@PathVariable("formUrl") String formUrl, ModelMap map){
-        //return "displayForm";
-        map.addAttribute("name", formUrl);
+        Archetype arch;
         try {
-            arch = service.findByName(formUrl);
+            arch = archService.findByName(formUrl);
         } catch (Exception ex) {
-            map.addAttribute("name", "formular se nenasel");//tady se pak doplni nejaky chybova stranka 
             Logger.getLogger(FormController.class.getName()).log(Level.SEVERE, null, ex);
-            return "showForm";           
+            return "index";           
         }
-        //form = gwt.
+        map.addAttribute("archetype", arch);
+        map.addAttribute("formObject", new DigitalForm());//formObject by se mel shodovat s nazvem promenne v twigu
         return "showForm";
+        
+        
+        
+    }
+    
+    @RequestMapping(value = "/form/{formUrl}", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute("formObject") Form form, ModelMap map){
+        try {
+            formService.createForm(form);
+        } catch (Exception ex) {
+            System.out.println("chyba pri ukladani formulare");
+            Logger.getLogger(FormController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "index";
     }
     
 }
